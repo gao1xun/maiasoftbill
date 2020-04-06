b840a788d75c3625de66e6f5fe97c1c949e9efc0
gao1xun <gao1xun@163.com>
2020-04-06 20:10:47 +0900

    up

--- src/components/layouts/SeikyushoContent.js
+++ src/components/layouts/SeikyushoContent.js
@@ -6,6 +6,8 @@ import SeikyshoBottom from './SeikyshoBottom'
 
 const styles = {
     grid: {
+        marginTop:10,
+        marginBottom:10,
     },
     paper : {
         marginTop:10,
@@ -16,6 +18,17 @@ const styles = {
 
 export default ()=>
     <Fragment>
+        <Grid container style={styles.grid}>
+            <Grid item xs={2} >
+            請求書の新規作成
+            </Grid>
+            <Grid item xs={1} >
+            ご案内
+            </Grid>
+            <Grid item xs >
+            請求書の作り方がわからない方はこちらをクリック
+            </Grid>
+        </Grid>
         <Grid container spacing={2} >
             <Grid item xs style={{backgroundColor:'red'}}>
                 <SeikyshoLeft styles={styles}/>
